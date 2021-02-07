3f3aef12d02dc350677466b1d15d2682de4d0026
Mal0 <diablothedev@outlook.com>
2021-02-07 20:01:41 +0000

    Update index.js

--- message-logger-bot/index.js
+++ message-logger-bot/index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client()
 const config = require("./config.json")
-const {token, prefix} = require("./config.json")
+const {token} = require("./config.json")
 const moment = require("moment")
 const fs = require("fs")
 
